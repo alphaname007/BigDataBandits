0846761bf3e4e45d246642aa7ceef520ccfa862c
alpha name <danielweissenberger2016@gmail.com>
2023-10-16 13:35:28 +0200

    Added Trend detection with Linear Regression

--- analize.py
+++ analize.py
@@ -1,5 +1,41 @@
-def get_Trends(Post, Stock, intervall_length:int=5):
-    start = Post.date
-    stop = start + intervall_length
+#from datetime import datetime
+import datetime
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def get_trade_day_avg(date, stock):
+    trade_day = stock.loc[stock['date'] == str(date.strftime('%Y-%m-%d'))]
+    
+    print(trade_day)
+
+    trade_day_open = trade_day.open.item()
+    trade_day_close = trade_day.close.item()
+    trade_day_avg = (trade_day_open + trade_day_close) / 2
+
+    return trade_day_avg
 
 
+def get_dates(date, interval_length:int=5):
+    dates = []
+
+    for i in range(0, interval_length):
+        date += datetime.timedelta(days=interval_length)
+        dates.append(date)
+
+    return dates
+
+
+def get_trend(date, stock, interval_length:int=5):
+    dates:list = get_dates(date, interval_length)
+    avgs:list = []
+
+    for date_i in dates:
+        avgs.append(get_trade_day_avg(date_i, stock))
+    
+    x = np.array([dates]).reshape((-1, 1))
+    y = np.array(avgs)
+
+    model = LinearRegression()
+    model.fit(x, y)
+
+    return model.coef_
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pandas as pd
-from datetime import datetime
+import datetime
 
-Tweets = pd.read_csv('elonmusk.csv')
+from analize import *
+
+Posts = pd.read_csv('elonmusk.csv')
 Tesla = pd.read_csv('tesla.csv')
 Dogecoin = pd.read_csv('dogecoin.csv')
 
-for i, Tweet in Tweets.iterrows():
-    date = datetime.strptime(str(Tweet["datetime"])[0:10], '%Y-%m-%d')
-    text = Tweet['text']
-    print(f'Elon Musk {date} ')
+for i, Post in Posts.iterrows():
+    #Convert Datetime-string, to Datetime-object
+    date_time = datetime.datetime.strptime(str(Post["datetime"])[0:18], '%Y-%m-%d %H:%M:%S')
+    date_time = date_time.strptime("2023-06-27 23:12:47", '%Y-%m-%d %H:%M:%S')
+    date = date_time.date()
+
+    print(get_trend(date, Tesla))
+
+    #text = Post['text']
+
+    #print(f'Elon Musk {date_time} ')
